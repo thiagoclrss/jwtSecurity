526c5ede4fb43bdccb593f8efb19b1e9b080db0a
thiagoclrss <thiagocolaress77@gmail.com>
2024-01-05 00:58:31 -0300

    first commit

---
+++ src/main/java/com/clrss/security/config/JwtService.java
@@ -0,0 +1,31 @@
+package com.clrss.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.websocket.Decoder;
+
+import java.security.Key;
+import java.util.Base64;
+
+public class JwtService {
+    private static final String SECRET_KEY = "4c0c74e2bb11070190ab331995c87c3c6b9731f4be03a3ee53b5b1c8a096f8ce";
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
