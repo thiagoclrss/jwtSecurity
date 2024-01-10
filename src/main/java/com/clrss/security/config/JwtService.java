1c2856bbb8a2ae789390cf984534a4be28c14512
thiagoclrss <thiagocolaress77@gmail.com>
2024-01-10 01:08:02 -0300

    update

--- src/main/java/com/clrss/security/config/JwtService.java
+++ src/main/java/com/clrss/security/config/JwtService.java
@@ -8,11 +8,17 @@ import jakarta.websocket.Decoder;
 
 import java.security.Key;
 import java.util.Base64;
+import java.util.function.Function;
 
 public class JwtService {
     private static final String SECRET_KEY = "4c0c74e2bb11070190ab331995c87c3c6b9731f4be03a3ee53b5b1c8a096f8ce";
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token){
